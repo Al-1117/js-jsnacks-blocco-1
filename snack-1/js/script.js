72b1e51eb0399956928d912fd03fed7b60660863
Alioune <alioune1117@gmail.com>
2020-05-28 16:46:34 +0200

    inseriti elementi html al primo e secondo snack

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,26 +1,32 @@
 // L’utente inserisce due numeri in successione,
  // con due prompt. Il software stampa il maggiore.
 
+// ELEMENTI HTML
+
+
+
+
 
 // Chiedo all'utente il primo numero
 var numeroUno = prompt("Dammi il primo numero");
 console.log(numeroUno);
+document.getElementById('primo_numero').innerHTML = numeroUno;
 // Chiedo all'utente il secondo numero
 var numeroDue = prompt("Dammi il secondo numero");
 console.log(numeroDue);
-
+document.getElementById('secondo_numero').innerHTML = numeroDue;
 // Stampo il numero maggiore
 
-var messaggio
+var messaggio = document.getElementById('messaggio');
 
 if (numeroUno > numeroDue) {
-  messaggio = alert("il primo numero è più grande")
+  messaggio.innerHTML = "il primo numero è più grande"
 
 } else if (numeroUno < numeroDue) {
-  messaggio = alert("il secondo numero è più grande")
+  messaggio.innerHTML = ("il secondo numero è più grande")
 
 } else {
-  messaggio = alert("i numeri sono uguali")
+  messaggio.innerHTML = ("i numeri sono uguali")
 }
 
 console.log(messaggio);
